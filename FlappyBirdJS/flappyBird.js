9b79d533c56f5f9bc66d74559a22051a8ff3622e
bluehabit <chris.feltus@gmail.com>
2018-01-26 14:16:12 -0600

    Update flappyBird.js

--- FlappyBirdJS/flappyBird.js
+++ FlappyBirdJS/flappyBird.js
@@ -19,11 +19,44 @@ pipeSouth.src = "images/pipeSouth.png";
 var gap = 85;
 var constant;
 
+var bx = 10;
+var by = 150;
+var gravity = 1;
+
+//keydown
+
+document.addEventListener('keydown', moveUp);
+
+function moveUp(){
+	by -= 20;
+}
+
+//pipe coordinates
+var pipe = [];
+
+pipe[0] = {
+	x: cvs.width,
+	y: 0
+}
+
+
 // //draw images
 function draw(){
 	ctx.drawImage(bg,0,0);
-	ctx.drawImage(pipeNorth, 100, 0);
-	ctx.drawImage(pipeSouth,100, 0 + constant);
+
+
+	for(var i = 0; i < pipe.length; i++){
+		ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+		ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+
+		//moves pipes to the left
+		pipe[i].x--;
+	}
+
+	ctx.drawImage(fg,0, cvs.height - fg.height);
+	ctx.drawImage(bird, bx, by);
+	by += gravity;
+	requestAnimationFrame(draw);
 }
 
 window.onload = function(){
